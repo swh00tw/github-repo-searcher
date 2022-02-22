87a385df187ce874820eafcdec93cedd4fe6fa43
swh00tw <a6140000@gmail.com>
2022-02-22 15:22:11 +0800

    complete homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,46 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import PageMotionContainer from '../components/PageMotionContainer';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, Heading, useColorModeValue, Input, InputRightElement, InputGroup, InputLeftElement, Divider, Button } from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { ArrowRightIcon } from '@chakra-ui/icons';
+import { useState, useEffect } from 'react';
 
 function Home({}) {
+  const [search, setSearch] = useState('');
+
   return (
     <PageMotionContainer>
-      <Flex w='100%' h='85vh' align='center' justify='center'>
-        <Box w='50%' h='50%' bg='teal.300' borderRadius={'3xl'}>
-          Search Box UI
+      <Flex w='100%' h='100%' py={150} align='center' justify='center'>
+        <Box w={{ base: '90%', md: '50%' }} h='60%' bg={useColorModeValue('cyan.500', 'cyan.700')} borderRadius={'3xl'} color='white' flexDirection={'column'}>
+          <Heading fontSize={{ base: '4xl', md: '6xl' }} mt={10} px={5}>
+            Type in Github username
+          </Heading>
+          <Heading fontSize={{ base: '4xl', md: '6xl' }} mt={10} px={5}>
+            & browse all repos 💫
+          </Heading>
+          <Flex w='80%' mt={10}>
+            <InputGroup my={10}>
+              <InputLeftElement pointerEvents='none' color='gray.300' fontSize='1.2em' children={<FaGithub />} />
+              <Input
+                fontFamily={'Monserrat'}
+                variant='filled'
+                placeholder='Enter username'
+                focusBorderColor='cyan.200'
+                onChange={e => {
+                  setSearch(e.target.value);
+                }}
+              />
+            </InputGroup>
+          </Flex>
+          <Flex justify='center' pb={10} px={10}>
+            <motion.div whileHover={{ scale: 1.2 }}>
+              <Button size='lg' variant='outline' rightIcon={<ArrowRightIcon />}>
+                Search
+              </Button>
+            </motion.div>
+          </Flex>
         </Box>
       </Flex>
     </PageMotionContainer>
