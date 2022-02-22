21ae75be88c8d2539413ab7ca961d516f5289850
swh00tw <a6140000@gmail.com>
2022-02-22 21:07:25 +0800

    fix animation presence

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,12 @@ import theme from '../theme/';
 import Layout from '../components/Layout';
 import { AnimatePresence } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
         <AnimatePresence exitBeforeEnter initial={true}>
-          <Component {...pageProps} />
+          <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Layout>
     </ChakraProvider>
