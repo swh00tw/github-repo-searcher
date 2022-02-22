2078aa063efd2a26cca1a5c7e56975658343cadc
swh00tw <a6140000@gmail.com>
2022-02-22 22:40:57 +0800

    modify repo card

--- components/RepoCard.js
+++ components/RepoCard.js
@@ -2,9 +2,9 @@ import { Box, Flex, Tag, Button, Heading, useColorModeValue, Divider } from '@ch
 import { ChevronRightIcon } from '@chakra-ui/icons';
 
 function RepoCard({ repo }) {
-  const { id, html_url, created_at, name, description } = repo;
+  const { id, html_url, updated_at, name, description, stargazers_count } = repo;
 
-  const timeTag = new Date(created_at);
+  const timeTag = new Date(updated_at);
   const year = timeTag.getFullYear();
   const month = (timeTag.getMonth() + 1).toString().padStart(2, '0');
   const day = timeTag.getDate().toString().padStart(2, '0');
@@ -25,25 +25,29 @@ function RepoCard({ repo }) {
             </Heading>
           </Flex>
           <Flex justifyContent={{ base: 'center', md: 'start' }} pt={0}>
-            <Tag borderRadius='full' mt={2} variant='solid' size='sm' colorScheme={useColorModeValue('yellow', 'teal')}>
-              {timeTagString}
+            <Tag borderRadius='full' mt={2} variant='solid' size='sm' colorScheme={'cyan'}>
+              Last update: {timeTagString}
+            </Tag>
+            <Tag ml={{ md: 2 }} borderRadius='full' mt={2} variant='outline' size='sm' colorScheme={'cyan'}>
+              🌟 {stargazers_count} stars
             </Tag>
           </Flex>
         </Flex>
         <Flex w={{ base: '100%', md: '30%' }} justify={{ base: 'center', md: 'end' }} py={4}>
           <Button
             rightIcon={<ChevronRightIcon />}
+            bg={useColorModeValue('cyan.300', 'cyan.600')}
             size='lg'
             onClick={() => {
               window.open(html_url, '_blank');
             }}
             w={{ base: '100%', md: '60%' }}
             _focus={{ border: 'none' }}>
-            Read
+            Github
           </Button>
         </Flex>
       </Flex>
-      <Divider w='60%' mt={3} mb={5} />
+      <Divider w='60%' mt={3} mb={5} borderWidth={2} />
     </>
   );
 }
